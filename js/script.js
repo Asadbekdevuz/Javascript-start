bdd980b37d0f723aac7c2b381471d07ac29f2515
Asadbekdevuz <mirxoliqovasad@gmail.com>
2022-07-29 09:12:42 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-var hero = alert("Xush kelibsiz! , Bilimingizni tekshirib ko'ramizmi");
+var hero = alert("Xush kelibsiz! , Bilimingizni tekshirib ko'ramizmi?");
 var userName = prompt("Ismingiz nima?");
 var question1 = prompt("HTML ning to'liq shaklini yozing!");
 var question2 = prompt("Inline va inline-block farqi nima?");
 var question3 = prompt("index.htmlga style.css ni qanday ulashni yozing!");
-var question4 = prompt("img atributlarini yozib bering");
-var question5 = prompt("Ul ga beriladigan stillarni yozing?");
+var question4 = prompt("img atributlarini yozib bering!");
+var question5 = prompt("ul ga beriladigan stillarni yozing?");
 var question6 = prompt("index.html ni script.js ga ulashni yozing!");
-var question7 = prompt("javascriptda malumot turlari");
+var question7 = prompt("javascriptda malumot turlarini ayting?");
 var question8 = prompt("stringni numberga qoshganda nima bo'ladi?");
 var question9 = prompt("alert bn promptni farqi nimada?");
 var question10 = prompt("console.log vazifasi nima?");
 var intro = confirm("Savollarimizdan mamnunmisiz!!");
-var things = alert(`Javoblaringiz uchun rahmat , ${userName}`);
+var things = alert(`Javoblaringiz uchun rahmat! , ${userName}`);
 
 
 
